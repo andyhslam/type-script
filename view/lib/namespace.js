acd199fd5dce6ed52c358290c2909c137b9394fa
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-03-15 12:41:25 +0800

    三斜线指令

---
+++ view/lib/namespace.js
@@ -0,0 +1,14 @@
+"use strict";
+var G;
+(function (G) {
+    G.g3 = 73;
+})(G || (G = {}));
+var G;
+(function (G) {
+    G.g4 = 74;
+})(G || (G = {}));
+var G;
+(function (G) {
+    G.g = 7;
+})(G || (G = {}));
+console.log(G);
