fc69b027f78bae616166fa7715ed585ca8d860ca
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-03-18 12:52:07 +0800

    TS进阶用法proxy & Reflect

--- src/index-rollup.ts
+++ src/index-rollup.ts
@@ -1,64 +1,37 @@
-// 实现发布订阅模式
-interface EventFace {
-  on: (name: string, fn: Function) => void,
-  emit: (name: string, ...args: Array<any>) => void,
-  off: (name: string, fn: Function) => void,
-  once: (name: string, fn: Function) => void,
+// TS进阶用法proxy & Reflect
+type Person1 = {
+  name: string,
+  age: number,
+  text: string
 }
-interface List {
-  [key: string]: Array<Function>
-}
-
-class Dispatch implements EventFace {
-  list: List;
-  constructor() {
-    this.list = {}
-  }
-  on(name: string, fn: Function) {
-    const callback = this.list[name] || []
-    callback.push(fn)
-    this.list[name] = callback
-    // console.log(this.list)
-  }
-  emit(name: string, ...args: Array<any>) {
-    let eventName = this.list[name]
-    if (eventName) {
-      eventName.forEach(fn => {
-        fn.apply(this, args)
-      })
-    } else {
-      console.error(`名字错误${name}`);
+// 实现事件监听器
+const proxy = (object: any, key: any) => {
+  return new Proxy(object, {
+    get(target, prop, receiver) {
+      console.log('get=>', prop);
+      return Reflect.get(target, prop, receiver)
+    },
+    set(target, prop, value, receiver) {
+      console.log('set=>', prop);
+      return Reflect.set(target, prop, value, receiver)
     }
-  }
-  off(name: string, fn: Function) {
-    let eventName = this.list[name]
-    if (eventName && fn) {
-      let index = eventName.findIndex(fns => fns === fn)
-      eventName.splice(index, 1)
-    } else {
-      console.error(`名字错误${name}`);
-    }
-  }
-  once(name: string, fn: Function) {
-    let tempFn = (...args: Array<any>) => {
-      fn.apply(this, args)
-      this.off(name, tempFn)
-    }
-    this.on(name, tempFn)
-  }
+  })
 }
 
-const dispatch = new Dispatch();
-// const fnc =  (...args: Array<any>) => {
-//   console.log(args, 2);
-// }
-dispatch.on('post', (...args: Array<any>) => {
-  console.log(args, 1);
-})
-dispatch.once('post', (...args: Array<any>) => {
-  console.log(args, 'once');
-})
-// dispatch.on('post', fnc)
-// dispatch.off('post', fnc)
-dispatch.emit('post', 1, false, { sex: 'female' });
-dispatch.emit('post', 2, true, { sex: 'female' });
+const logAccess = <T>(object: T, key: keyof T): T => {
+  return proxy(object, key);
+}
+
+const girl: Person1 = logAccess({
+  name: 'lx',
+  age: 18,
+  text: '女中豪杰'
+}, 'name')
+
+const boy = logAccess({
+  id: 10,
+  name: 'andy'
+}, 'id')
+
+girl.age;
+console.log(girl);
